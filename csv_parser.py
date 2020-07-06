3aa346c128fe11176a277e6a1c2be5f3c7545884
Vadim Work <vadim.vasilinenko@eltex-co.ru>
2020-07-06 17:57:32 +0700

    master

--- csv_parser.py
+++ csv_parser.py
@@ -3,8 +3,14 @@
 # lib
 import csv
 
-with open('../result_file/issues21333222.csv', 'r') as csvfile:
-    csv_read = csv.reader(csvfile, delimiter = ';')
-    for line in csv_read:
-        print(line[2])
+csv_file_path = '../result_file/issues21333222.csv'
+
+def read_csv_file(number_column):
+    with open(csv_file_path, 'r') as csvfile:
+        csv_read = csv.reader(csvfile, delimiter = ';')
+        for line in csv_read:
+            return (line[number_column])
+
+read_csv_file(2)
+
 
