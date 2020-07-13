2248dd672002a3ec63693f3e363375742e861c66
Vadim Work <vadim.vasilinenko@eltex-co.ru>
2020-07-13 18:04:26 +0700

    up

--- csv_parser.py
+++ csv_parser.py
@@ -5,7 +5,7 @@
 # lib
 import csv
 
-csv_file_path = 'issues21333222.csv'
+csv_file_path = '../result_file/issues21333222.csv'
 
 
 def parser_csv_file():
@@ -40,43 +40,7 @@ for i in parser_csv_file():  # Строим Проект
                                 dict_s2[k[arg5_info]] = [k[arg4_info], k[arg3_info]]
                     dict_s1[str(i[arg1_info])][j[arg2_info]] = dict_s2
                     dict_s2 = {}
+#print(dict_s1)
 
-print(dict_s1)
-
-# МУСОР
-
-# def read_csv_file(number_column):
-#     with open(csv_file_path, 'r') as csvfile:
-#         csv_read = csv.reader(csvfile, delimiter = ';')
-#        #number = 0
-#         my_list = []
-#         next(csv_read) #переход на следующие значение
-#         for line in csv_read:
-#             print (line[number_column])
-#             my_list.append(line[number_column])
-#     print('Количество элементов = ', len(my_list)) #Длинна списка
-#     print(my_list) # Показать список
-# read_csv_file(2)
-
-
-# with open(csv_file_path, 'r') as csvfile:
-#     csv_read = csv.DictReader(csvfile)
-#     for row in csv_read:
-#         print(row['Версия'])
-
-
-# print(parser_csv_file(arg_info))
-# for row in parser_csv_file():
-#     print(row[arg_info])
-
-
-# with open(csv_file_path, 'r') as csvfile:
-#     csv_read = csv.DictReader(csvfile)
-#     i = 0
-#     for row in csv_read:
-#         print(row)
-#         print('-------------------line_------------------------------------')
-#         i = i + 1
-#         print(i)
-#         print(row['Проект'])
-#         print('-------------------line_------------------------------------')
+#  Подсчет количества Проектов
+print(dict_s1.keys())
