5c641a76b81579b9b6eb5af0e2feb1e6d5bdc732
sayanss2 <kolosovsky.al@yandex.ru>
2020-07-21 18:40:17 +0700

    add new functions

--- csv_parser.py
+++ csv_parser.py
@@ -5,7 +5,7 @@
 # lib
 import csv
 
-csv_file_path = '../result_file/issues21333222.csv'
+csv_file_path = 'issues21333222.csv'
 
 
 def parser_csv_file():
@@ -17,8 +17,6 @@ def parser_csv_file():
     return lines
 
 
-
-
 arg1_info = 'Проект'
 arg2_info = 'Версия'
 arg3_info = 'Статус'
@@ -27,15 +25,13 @@ arg5_info = '#'
 dict_s1 = {}  # Словарь по Проект:Версия
 dict_s2 = {}
 
-
-
 for i in parser_csv_file():  # Строим Проект
     if str(i[arg1_info]) in dict_s1.keys():
         pass
     else:
         dict_s1[i[arg1_info]] = {}
-        for j in parser_csv_file():   # Строим Версию
-            if str(i[arg1_info]) in j[arg1_info]:   # Проверка по Проекту
+        for j in parser_csv_file():  # Строим Версию
+            if str(i[arg1_info]) in j[arg1_info]:  # Проверка по Проекту
                 if dict_s1.get(str(i[arg1_info])).get(j[arg2_info], {}):
                     pass
                 else:
@@ -45,23 +41,19 @@ for i in parser_csv_file():  # Строим Проект
                                 dict_s2[k[arg5_info]] = [k[arg4_info], k[arg3_info]]
                     dict_s1[str(i[arg1_info])][j[arg2_info]] = dict_s2
                     dict_s2 = {}
-#print(dict_s1)
 
+
+# print(dict_s1)
 
 
 #  Подсчет количества Проектов
-#print(dict_s1.keys())
-#print(dict_s1.keys())
+# print(dict_s1.keys())
+# print(dict_s1.keys())
 # key = list(dict_s1.keys())[0]
 # val = list(dict_s1.get(key).keys())[0]
 
 
-# for key in dict_s1:
-#     for val in dict_s1.get(key):
-#         print(key, '/', val)
-
-
-def get_cntProjectVersion():
+def get_cnt_project_version():
     cnt = 0
     for key in dict_s1:
         for val in dict_s1.get(key):
@@ -69,15 +61,47 @@ def get_cntProjectVersion():
     return cnt
 
 
-def get_ProjectVersion():
-    verList = []
-    for key in dict_s1:
-        for val in dict_s1.get(key):
-            verList.append(key+'/'+val)
-    return verList
+def get_project_version(return_sep=None):
+    ver_list = []
+    if return_sep:
+        for key in dict_s1:
+            for val in dict_s1.get(key):
+                ver_list.append(key + '/' + val)
+    else:
+        for key in dict_s1:
+            for val in dict_s1.get(key):
+                line_str = key + '/' + val
+                dict_str = {'oneline': line_str, 'project': key, 'version': val}
+                ver_list.append(dict_str)
+    return ver_list
 
-print('Count = ', get_cntProjectVersion())
-print(*get_ProjectVersion(), sep='\n')
+
+def get_order_project_version(project_input=None, version_input=None):
+    order_list = []
+    if project_input and version_input:
+        for key, value in dict_s1.get(project_input).get(version_input).items():
+            url_value = 'http://red.eltex.loc/issues/' + key + ' (' + value[0] + ')'
+            status_value = value[1]
+            dict_str = {'url': url_value, 'status': status_value}
+            order_list.append(dict_str)
+    return order_list
+
+
+def get_cnt_order(project_input=None, version_input=None):
+    cnt = 0
+    if project_input and version_input:
+        for item in dict_s1.get(project_input).get(version_input).items():
+            cnt += 1
+    return cnt
+
+
+print('Count = ', get_cnt_project_version())  # Вывод кол-ва Проектов/Версий
+for i in get_project_version():
+    print(i.get('oneline'))  # Вывод наименования Проекта/Версии одной строкой
+    print('Count = ', get_cnt_order(i.get('project'), i.get('version')))  # Вывод кол-ва задач в Проекте/Версии
+    for j in get_order_project_version(i.get('project'), i.get('version')):
+        print(j.get('url') + ' ' + j.get('status'))  # Вывод ссылки, наименования и статуса задачи в Проекте/Версии
+
 
 # Нужна функция получения списка задач, номера от конткретного Проекта/Версии
 # При этом сначала должен идти http://red.eltex.loc/issues/НОМЕР_ЗАДАЧИ (НАЗВАНИЕ ЗАДАЧИ)
