057d501708ba905133ebc70c3d5219f90a0d2414
Vadim Work <vadim.vasilinenko@eltex-co.ru>
2020-07-27 17:17:35 +0700

    new date and mouth

--- red_api.py
+++ red_api.py
@@ -3,26 +3,31 @@
 import redminelib
 import os
 import csv
-
+import shutil
+import datetime
+from dateutil.relativedelta import relativedelta
 
 # - functions
 # def print_list_red(x_arg_input):
 #     for data in x_arg_input:
 #         print(data)
-
+today = datetime.date.today()
+mouth_report =today + relativedelta(months=-1)
+mouth_plan =today + relativedelta(months=+1)
 
 # - auth
 red = redminelib.Redmine('http://red.eltex.loc', key='01ec8ef40c680c06b32e7201dfd80c328803ee4b')
 # print(' auth +', red.url, '\n','Ваше Имя:', red.user.get('current')) #Получение адреса и имени пользователя
 # print(' Ваш ID:', red.user.get('current').id) #Получение ID пользователя
 
+# issue_custom_field_values_2 - Quality Assurance 6 статус это Rejected
 issues = red.issue.filter(cf_2='me', status_id='!6',
-                          updated_on='>=2020-06-28')  # issue_custom_field_values_2 - Quality Assurance 6 статус это
-# Rejected
+                          updated_on='>='+str(mouth_report))  
 
 # print_list_red(issues)
 
-# Выгрузка для linux
+# Выгрузка в csv
+shutil.rmtree('artifacts/', ignore_errors=True)
 if not os.path.exists('artifacts/'):
     os.makedirs('artifacts/')
     issues.export('csv', savepath='artifacts/', filename='issues_control.csv',
